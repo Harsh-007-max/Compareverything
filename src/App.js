5eb362a7cb4b6dd99b62d8c67486749201ca2779
Harsh-007-max <harshbhalodiya1209+github@gmail.com>
2023-02-03 12:41:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Searchbar from './components/Searchbar';
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743"; //optional color #212529
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Searchbar title="Compareverything" hometxt="" linktxt="" disabledtxt="" toggleMode={toggleMode} mode={mode}/>
+    </>
   );
 }
 
---
+++ src/components/Searchbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import "./CSS/Searchbar.css"
+import search from "./logo/search.png"
+import dark from "./logo/Dark-Mode.png"
+import light from "./logo/Light-Mode.png"
+import login from "./logo/login-logo.png"
+export default function Searchbar(props) {
+  return (
+    <div>
+        <nav>
+            <div className={`d-flex navbar-${props.mode} ${props.mode==='light'?'gradient-background-light':'gradient-background-dark'}`}>
+                {/* Search bar code is given below */}
+                <a className='navbar-brand p-3 custom-bold' href='/'>{props.title}</a>
+                <form className="d-flex mx-auto pt-2 pb-2" role="search">
+                    {/* form-control */}
+                    <input className='d-flex sb me-2' type="search" placeholder='Search product' aria-label='Search'/>
+                    <button className='btn seb ' type='submit'><img src={search} alt="" /></button>
+                </form>
+                {/* Login / Sign Up page code below */}
+                    <div className='d-flex lg'>
+                        <button className="btn">
+                            <img src={login} alt="Login" />
+                            <label className=''>login</label>
+                        </button>
+                    </div>
+                {/* Light to dark mode switch code below */}
+                <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} p-3`}>
+                    <input className="form-check-input " onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> <img src={props.mode==='light'? light:dark} alt="" /></label>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
