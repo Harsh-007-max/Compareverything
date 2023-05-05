3b635eb145028d624d9b93562673b4a305abca80
Harsh <harshbhalodiya1209+github@gmail.com>
2023-05-05 05:42:58 +0530

    about us page completed

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,7 +9,7 @@ function AboutUs(props) {
       .then((data) => setData(data))
       .catch((err) => console.log(err));
   }, []);
-  const backgroundStyle={
+  const backgroundStyle = {
     backgroundImage: `url(${AboutImage})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
@@ -31,17 +31,39 @@ function AboutUs(props) {
   };
   return (
     <>
-    <div className="about-us" style={backgroundStyle}>
-      <div className="about-us-content" style={overlayStyle}>
-        <htmlH1>`About Us`</htmlH1>
-        
-      </div>
-    </div>
-    {data.map((item, index) => (
-      <div key={index} className="about-us-section">
+      <div className="about-us" style={backgroundStyle}>
+        <div className="about-us-content" style={overlayStyle}>
+          <htmlH1>`About Us`</htmlH1>
 
+        </div>
       </div>
-    ))}
+      
+      <div className={`aboutus__data ${props.mode==='light'?'aboutus__data__light':'aboutus__data__dark'}`}>
+      <h1>INFORMATION</h1>
+      <p>{`This website for purchasing products to help compare the price from various e-commerce websites, this Price comparison site is extremely helpful for frequent online shoppers to check prices on different online stores in one place, this system will show you the product prices from different retailers to show you where to buy the product at affordable price.`}</p>
+      <hr/>
+      <h1>CHARACTERISTICS</h1>
+      <ul>
+      <li>It saves time. It became easy to shop in affordable prices
+      </li>
+      <li>Reliable for interaction.</li>
+      <li>Efficient work</li>
+      </ul>
+      <hr/>
+      <h1>Contact Admin :</h1>
+      {data.map((item, index) => (
+        <div key={index} className="about-us-section">
+          <ul>
+            <li>Name : {item.AdminName}<br/>
+            <ul>
+            Email : {item.Email}<br/>
+            Mobile Number : {item.MobileNumber}
+            </ul></li>
+          </ul>
+        </div>
+      ))}
+      </div>
+
     </>
   )
 }
